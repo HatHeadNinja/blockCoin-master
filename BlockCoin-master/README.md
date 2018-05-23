672eea9ef056eb66b11155d079695714ae9dc934
HatHeadNinja <HatHeadNinja@users.noreply.github.com>
2018-05-23 12:28:40 -0700

    Cleaned out redundant code
    There were 3 progressive versions of the script. Deleted earlier 2 and left most recent.

--- BlockCoin-master/README.md
+++ BlockCoin-master/README.md
@@ -1,10 +1,8 @@
 # BlockCoin
 
-A very simple blockchain implementation to demonstrate the basics of blockchains. 
+A very simple blockchain implementation to demonstrate the basics of blockchains: hashing, proof-of-work, and rewarding incentives ('mining'). 
 
 This is by no means a complete implementation and it is by no means secure!
 
 Output from this great JavaScript tutorial:
 https://www.youtube.com/watch?v=zVqczFZr124
-
-main-v3.js is the most 'complete', including hashing, proof-of-work, and rewards/incentivizing
--- BlockCoin-master/src/main.js
+++ BlockCoin-master/src/main.js
@@ -1,16 +1,33 @@
 const SHA256 = require("crypto-js/sha256");
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = '') {
         this.previousHash = previousHash;
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-      return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("BLOCK MINED: " + this.hash);
     }
 }
 
@@ -18,20 +35,51 @@ class Block {
 class Blockchain{
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock() {
-        return new Block(0, "01/01/2017", "Genesis block", "0");
+        return new Block(Date.parse("2017-01-01"), [], "0");
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
+
+        console.log('Block successfully mined!');
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isChainValid() {
@@ -53,16 +101,15 @@ class Blockchain{
 }
 
 let blockCoin = new Blockchain();
-blockCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
-blockCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
+blockCoin.createTransaction(new Transaction('address1', 'address2', 100));
+blockCoin.createTransaction(new Transaction('address2', 'address1', 50));
 
+console.log('\n Starting the miner...');
+blockCoin.minePendingTransactions('dougs-address');
 
-console.log('Blockchain valid? ' + blockCoin.isChainValid());
+console.log('\nDougs balance is', blockCoin.getBalanceOfAddress('dougs-address'));
 
-console.log('Changing a block...');
-blockCoin.chain[1].data = { amount: 100 };
-// blockCoin.chain[1].hash = blockCoin.chain[1].calculateHash();
+console.log('\n Starting the miner again...');
+blockCoin.minePendingTransactions('dougs-address');
 
-console.log("Blockchain valid? " + blockCoin.isChainValid());
-
-// console.log(JSON.stringify(blockCoin, null, 4));
+console.log('\nDougs balance is', blockCoin.getBalanceOfAddress('dougs-address'));
